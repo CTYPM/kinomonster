4fb30a9ce96402bc1f9934a846d83ed3dabaa501
Andrey <a.n.bogach@yandex.ru>
2018-12-06 21:30:43 +0300

    Работа с формами

--- index.php
+++ index.php
@@ -1,57 +1,25 @@
 <?php 
-
-$arr = array("Рэмбо: Первая кровь", "Звездный путь", "Серенити", "Придорожная закусочная", "Книга Илая");
-
-array_push($arr, "Ответный удар");
-
-$arr2 = array("Исходный код", "Патруль времени");
-
-
-$newArr = array_merge($arr, $arr2);
-
-foreach ($newArr as $key => $value) {
-	echo $value . "<br>";
+print_r($_POST);
+// if (isset($_POST['Name'])){
+// echo "Мое имя " . $_POST['Name'];
 }
-
-// $str = implode(", ", $newArr);
-// echo "<br>" . $str;
-
-$str = "Яблоко, Груша, Тыква, Арбуз";
-$strToArr = explode(", ", $str);
-echo "<pre>";
-print_r($strToArr);
-echo "</pre><br><br>";
-
-
-// $camera["market1"]["row1"][0] = "Яблоко";
-// $camera["market1"]["row1"][1] = "Груша";
-// $camera["market1"]["row2"][0] = "Тыква";
-// $camera["market1"]["row2"][1] = "Ананас";
-// $camera["market2"]["row1"][0] = "Слива";
-// $camera["market2"]["row2"][0] = "Кокос";
-// $camera["market1"]["row2"][1] = "Арбуз";
-
-$camera = array(
-	"market1" => array(
-		"row1" => array("Яблоко", "Груша"),
-		"row2" => array("Тыква", "Ананас")
-	),
-	"market2" => array(
-		"row1" => array("Слива", "Колбаса"),
-		"row2" => array("Сосиски", "Сыр")
-	)	
-);
-
-
-echo "<pre>";
-foreach ($camera as $key => $value) {
-	foreach ($value as $key1 => $value1) {
-		foreach ($value1 as $key2 => $value2) {
-			echo $key . ">" . $key1 . ">" . $key2 . " = " . $value2 . "<br>";
-		}
-	}
-}
-echo "</pre><br><br>";
 
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<form action="/" method="post">
+		<input type="text" name="Name" placeholder="Ваше имя...">
+		<br>
+		<textarea name="text" id=""></textarea>
+		<input type="hidden" name="ID_User" value="1111">
+		<input type="submit" value = "Отправить">
+		<br>
+	</form>
+</body>
+</html>
