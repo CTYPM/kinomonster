aae0d3245ea764c10bed40e57220528f2e601369
CTYPM <a.n.bogach@yandex.ru>
2018-12-07 16:45:16 +0300

    Функции

--- index.php
+++ index.php
@@ -1,30 +1,15 @@
 <?php 
-//print_r($_POST);
-if (isset($_POST['Name'])){
-$nameFilter = htmlspecialchars(mb_strtolower(trim($_POST['Name'])),ENT_QUOTES, "UTF-8");
-echo "Мое имя " . $nameFilter;
-echo $_POST['text'];
+function summary($a, $b){
+	$result = "";
+	if(!is_numeric($a) || !is_numeric($b)) {
+		$result = "Заданы неверные данные";
+	} else {
+		$result = $a + $b;
+	}
+	return $result;
 
-$str = "<br>" . wordwrap($_POST['text'], 10, "<br>", true);
-echo $str;
-}
+	}
+echo summary(3, 5);
 
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Document</title>
-</head>
-<body>
-	<form action="/" method="post">
-		<input type="text" name="Name" placeholder="Ваше имя...">
-		<br>
-		<textarea name="text" id=""></textarea>
-		<input type="hidden" name="ID_User" value="1111">
-		<input type="submit" value = "Отправить">
-		<br>
-	</form>
-</body>
-</html>
